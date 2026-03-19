55798fcc4192ea40946603ce6f88c765f1098dbf
Guilherme Moura <glhrmoura@gmail.com>
2026-03-19 10:19:27 -0300

    feat: register service worker at startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 import './index.css';
 import './i18n';
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
